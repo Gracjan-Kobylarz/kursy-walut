2c2974c5ba7f25288e450e001c642408f8bceb5b
Gracjan Kobylarz <gracjan.omnioxy@gmail.com>
2022-10-13 08:47:07 +0200

    code converted to ES5 version

--- main.js
+++ main.js
@@ -1,51 +1,58 @@
-const currentExchange = document.querySelector('.currency-list');
-const currencyHistory = document.querySelector('.currency-history');
-const currencySelected = document.querySelector('.selected-currency');
-let historyURL;
-let value;
-let selectedCurrency;
-let index = 0;
+var currentExchange = document.getElementsByClassName('currency-list');
+var currencyHistory = document.getElementsByClassName('currency-history');
+var currencySelected = document.getElementsByClassName('selected-currency');
+var value;
+var currency;
+var selectedCurrency;
+var historyData;
+var historyDataResult;
 
 
-const keyValue = (input) => Object.entries(input).forEach(([key, value]) => {
-  const li = document.createElement("li");
+
+function keyValue(value) {
+
+  var currencyList = Object.entries(value);
+  currencyList.forEach(function(item, index) {
+    var li = document.createElement("li");
   li.classList.add('currency-list__item');
-  currentExchange.appendChild(li).innerHTML = `<span id='${index}'><b>${key}</b> ${value} </span>`;
-  index++;
-})
-
-const historyValue = (input) => Object.entries(input).forEach(([key, value]) => {
-  const li = document.createElement("li");
-  li.classList.add('history-list__item');
-  currencyHistory.appendChild(li).innerHTML = `<span><b>${key}</b>  ${value} </span>`;
-})
-
-const requestURL = `https://api.fastforex.io/fetch-all?from=PLN&api_key=ec8f65a590-026c86e3e9-rjbjvg`;
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json'
-  }
-};
-
-async function exchange() {
-  const response = await fetch(requestURL, options);
-  const currency = await response.json();
-  value = currency.results;
-  keyValue(value);
-}
-exchange();
-
-async function historyExchange(item) {
-  let historyURL = `https://api.fastforex.io/time-series?from=PLN&to=${currency}&start=2022-10-01&end=2022-10-06&api_key=ec8f65a590-026c86e3e9-rjbjvg`;
-  const response = await fetch(historyURL, options);
-  const historyData = await response.json();
-  const historyDataResult = historyData.results[`${currency}`];
-  currencyHistory.innerHTML = "";
-  historyValue(historyDataResult);
+  currentExchange[0].appendChild(li).innerHTML = '<span id=' + index + '><b>' + item[0] + '</b>' + item[1] + '</span>';
+  });
+  
 }
 
-const onClick = (event) => {
+function historyValue(value) {
+  var historyList = Object.entries(value);
+  historyList.forEach(function(item, index) {
+    var lit = document.createElement("li");
+  lit.classList.add('history-list__item');
+  currencyHistory[0].appendChild(lit).innerHTML = '<span id=' + index + '><b>' + item[0] + '</b>' + item[1] + '</span>';
+  });
+  
+}
+
+var options = {method: 'GET', headers: {accept: 'application/json'}};
+
+fetch('https://api.fastforex.io/fetch-all?from=PLN&api_key=ec8f65a590-026c86e3e9-rjbjvg', options) 
+  .then(function(response) {return response.json();})
+  .then(function(data) {value = data.results;})
+  .then(function() {keyValue(value);})
+
+  .catch(function(error) {console.error(error);});
+
+
+function historyExchange(currency) {
+
+fetch('https://api.fastforex.io/time-series?from=PLN&to=' + currency + '&start=2022-10-08&end=2022-10-13&api_key=ec8f65a590-026c86e3e9-rjbjvg', options) 
+  .then(function(response) {return response.json();})
+  .then(function(data) {historyData = data.results;})
+  .then(function() {historyDataResult = historyData[currency];})
+  .then(function() {currencyHistory[0].innerHTML = "";})
+  .then(function() {historyValue(historyDataResult);})
+  
+  .catch(function(error) {console.error(error);});
+}
+
+function selectCurrency(event)  {
   selectedCurrency = event.srcElement.id;
   currency = Object.keys(value)[selectedCurrency];
   if (typeof currency === 'undefined') {
@@ -53,7 +60,7 @@ const onClick = (event) => {
   } else {
     currency = Object.keys(value)[selectedCurrency];
   }
-  currencySelected.textContent = `PLN : ${currency}`;
+  currencySelected[0].textContent = 'PLN :' + currency;
   historyExchange(currency);
 }
-currentExchange.addEventListener('click', onClick);
+currentExchange[0].addEventListener('click', selectCurrency);
